fc569c9179d94b4847517faed67af7180dce9154
nina-thur <74824565+nina-thur@users.noreply.github.com>
2021-02-22 08:02:05 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Weather from "./Weather";
+import App from "./App";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Weather />
+      <App />
     </div>
   );
 }
