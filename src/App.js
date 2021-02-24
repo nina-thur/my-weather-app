257c312553d085ee1dfadeeb4b1adc34509ae5e3
nina-thur <74824565+nina-thur@users.noreply.github.com>
2021-02-24 13:13:52 +0100

    Created App.js
    Moved over some code to make it a better structure

---
+++ src/App.js
@@ -0,0 +1,31 @@
+ import React from "react";
+ import Weather from "./Weather";
+ import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <Weather defaultCity="Stockholm" />
+
+        <footer>
+          This project was coded by Nina Thür
+          and is{" "}
+          <a
+            href="https://github.com/nina-thur/my-weather-app.git"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://stupefied-agnesi-8a2e55.netlify.app"
+            target="_blank"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
+}
