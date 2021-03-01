547a824a1dd1e39c028fc2fc3680db2eb82670d7
nina-thur <74824565+nina-thur@users.noreply.github.com>
2021-03-01 18:41:58 +0100

    Imported my code from Codesandbox

--- src/Weather.js
+++ src/Weather.js
@@ -1,70 +1,60 @@
-import React, { useState } from "react";
-import WeatherInfo from "./WeatherInfo";
-import WeatherForecast from "./WeatherForecast";
+import React, { useState } from "react"; 
 import axios from "axios";
 import "./Weather.css";
+import WeatherForecast from "./WeatherForecast";
+import WeatherInfo from "./WeatherInfo";
 
-export default function Weather(props) {
-  const [weatherData, setWeatherData] = useState({ ready: false });
-  const [city, setCity] = useState(props.defaultCity);
 
-  function handleResponse(response) {
-    setWeatherData({
-      ready: true,
-      temperature: response.data.main.temp,
-      humidity: response.data.main.humidity,
-      date: new Date(response.data.dt * 1000),
-      description: response.data.weather[0].description,
-      icon: response.data.weather[0].icon,
-      wind: response.data.wind.speed,
-      city: response.data.name,
+export default function Weather (props){
+    
+    const [weatherData, setWeatherData] = useState ({ ready: false });
+    const [city,setCity]= useState (props.defaultCity);
+    function handleResponse (response){
+        setWeatherData ({
+            ready: true,
+            temperature: response.data.main.temp,
+            humidity: response.data.main.humidity,
+            date: new Date (response.data.dt*1000),
+            description: response.data.weather[0].description,
+            icon: response.data.weather[0].icon,
+            wind: response.data.wind.speed,
+            city: response.data.name,
     });
-  }
+}
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    search();
-  }
-
-  function handleCityChange(event) {
-    setCity(event.target.value);
-  }
-
-  function search() {
-    const apiKey = "94d579fc8052da3a293a4105275fe741";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search () {
+const apiKey="329ed4c9b33de1b60e6d184aac65a30a";
+    let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
-  }
+}
+function handleSubmit (event) {
+    event.preventDefault ();
+search ();
+}
 
-  if (weatherData.ready) {
+function handleCityChange (event) {
+setCity (event.target.value);
+}
+    if (weatherData.ready) {
     return (
-      <div className="Weather">
+    <div className="Weather">
         <form onSubmit={handleSubmit}>
-          <div className="row">
-            <div className="col-9">
-              <input
-                type="search"
-                placeholder="Enter a city.."
-                className="form-control"
-                autoFocus="on"
-                onChange={handleCityChange}
-              />
+            <div className="row">
+            <div className="col-9"> 
+            <input type="search" placeholder="Enter a city..." className="form-control" autoFocus="on" onChange={handleCityChange}/>
             </div>
             <div className="col-3">
-              <input
-                type="submit"
-                value="Search"
-                className="btn btn-primary w-100"
-              />
+        <button className="btn btn-primary" w-100> <i class="fas fa-search"></i></button>
+</div>
             </div>
-          </div>
         </form>
-        <WeatherInfo data={weatherData} />
-        <WeatherForecast city={weatherData.city} />
-      </div>
-    );
-  } else {
-    search();
-    return "Loading...";
-  }
+        <WeatherInfo data={weatherData}/>
+        <WeatherForecast city={weatherData.city}/>
+        
+    </div>
+    ); 
+} else {
+    search ();
+    return "Loading..."; 
+    }
 }
--- src/WeatherForecastPreview.js
+++ src/WeatherForecastPreview.js
@@ -1,24 +1,22 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
 
-export default function WeatherForecastPreview(props) {
-  function hours() {
-    let date = new Date(props.data.dt * 1000);
-    let hours = date.getHours();
-    return `${hours}:00`;
-  }
+export default function WeatherForecastPreview (props) {
+    function hours () {
+        let date = new Date (props.data.dt * 1000);
+        let hours = date.getHours ();
+        return `${hours}:00`; 
+    }
 
-  function temperature() {
-    let temperature = Math.round(props.data.main.temp);
-
-    return `${temperature}°C`;
-  }
-
-  return (
+    function temperature () {
+        let temperature = Math.round (props.data.main.temp);
+        return `${temperature}°C`;
+    }
+    return(
     <div className="WeatherForecastPreview col">
-      {hours()}
-      <WeatherIcon code={props.data.weather[0].icon} />
-      {temperature()}
-    </div>
-  );
+     {hours ()}
+        <WeatherIcon code={props.data.weather[0].icon} />
+        {temperature ()}
+      </div> 
+    );
 }
